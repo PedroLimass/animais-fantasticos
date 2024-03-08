9b4cf61dc319e8420941348e0397587ce3be58f8
Pedro Lima <phlimas@outlook.com>
2024-03-08 02:00:28 -0300

    feat:Criação do modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabNav from './module/initTabNav.js';
 import initAccordion from './module/initAccordion.js';
 import initScrollSuave from './module/initScrollSuave.js';
 import initAnimacaoScroll from './module/initAnimacaoScroll.js';
+import initModal from './module/initModal.js';
 
 initAccordion(); //Accordion List   
 initTabNav(); //Navegação por Tabs
 initScrollSuave(); //initScrollSuave
 initAnimacaoScroll(); //initAnimacaoScroll  
+initModal(); //initModal
