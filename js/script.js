fc208586fd6568fd65ff35e5b4028a10078174ef
Pedro Lima <phlimas@outlook.com>
2024-03-11 16:11:47 -0300

    feat:Criação da tooltip no map da aplicação

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from './module/initAccordion.js';
 import initScrollSuave from './module/initScrollSuave.js';
 import initAnimacaoScroll from './module/initAnimacaoScroll.js';
 import initModal from './module/initModal.js';
+import initTooltip from './module/initTooltip.js';
 
 initAccordion(); //Accordion List   
 initTabNav(); //Navegação por Tabs
 initScrollSuave(); //initScrollSuave
 initAnimacaoScroll(); //initAnimacaoScroll  
 initModal(); //initModal
+initTooltip(); //initTooltip
